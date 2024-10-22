28f2e9907a14d000cecb510ed52fd5ea2a796f5c
shind <shind@LENOVO>
2024-10-22 14:09:06 +0530

    Add project files.

---
+++ Statickeyword/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Statickeyword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student("pratik", 21);
+            Student s2 = new Student("pratik", 21);
+
+            Console.WriteLine(s1.display());
+            Console.WriteLine(s2.display());
+        }
+    }
+}
---
+++ Statickeyword/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Statickeyword
+{
+    public class Student
+    {
+        private int id;
+        private string name;
+        private int age;
+        private static int count;
+
+        static Student()
+        {
+            count = 0;
+        }
+
+        public Student( string name, int age)
+        {
+            count++;
+            this.id = count;
+            this.name = name;
+            this.age = age;
+        }
+
+        public string display()
+        {
+           return $"Id={id}/Name={name}/age={ age} ";
+        }
+    }
+}
